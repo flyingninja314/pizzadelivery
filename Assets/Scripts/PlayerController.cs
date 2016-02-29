fc3926829eaf5d8cc7d94f2ec4f23a35806445dc
Daniel Wright <daniel.wright.314@gmail.com>
2016-02-29 13:20:51 -0800

    initial plus movement and double jumping yo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float moveSpeed;
+	public float jumpHeight;
+	
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+
+	private bool doubleJumped;
+	
+	private Rigidbody2D myRigidBody2D;
+	
+	// Use this for initialization
+	void Start () {
+		myRigidBody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate() {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (grounded) {
+			doubleJumped = false;
+		}
+		if(Input.GetKeyDown (KeyCode.W) && grounded) {
+			//myRigidBody2D.velocity = new Vector2(myRigidBody2D.velocity.x, jumpHeight);
+			Jump ();
+		}
+		if(Input.GetKeyDown (KeyCode.W) && !doubleJumped && !grounded) {
+			//myRigidBody2D.velocity = new Vector2(myRigidBody2D.velocity.x, jumpHeight);
+			Jump ();
+			doubleJumped = true;
+		}
+		if(Input.GetKey (KeyCode.D)) {
+			myRigidBody2D.velocity = new Vector2(moveSpeed, myRigidBody2D.velocity.y);
+		}
+		if(Input.GetKey (KeyCode.A)) {
+			myRigidBody2D.velocity = new Vector2(-moveSpeed, myRigidBody2D.velocity.y);
+		}
+	}
+
+	public void Jump() {
+		myRigidBody2D.velocity = new Vector2(myRigidBody2D.velocity.x, jumpHeight);
+	}
+}
