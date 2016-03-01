2e68fedcbc2b921a4578a178454753c9af765c1f
Daniel Wright <daniel.wright.314@gmail.com>
2016-03-01 12:42:31 -0800

    checkpoints n spikes yo

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class Checkpoint : MonoBehaviour {
 
+	public LevelManager levelManager;
+	
 	// Use this for initialization
 	void Start () {
-	
+		levelManager = FindObjectOfType<LevelManager> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.name == "Player") {
+			levelManager.currentCheckpoint = gameObject;
+		}
 	}
 }
