f72b724bc62b94cd0ff4369de8c607a22bfe5c1d
Daniel Wright <daniel.wright.314@gmail.com>
2016-03-06 08:05:53 -0800

    fixing the camera. for example: you dont move if the enemy kills you

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -12,13 +12,17 @@ public class LevelManager : MonoBehaviour {
 
 	public int pointPenaltyOnDeath;
 
-	private float gravityStore;
-
 	public float respawnDelay;
+
+	private CameraController camera;
+	
+	private float gravityStore;
 
 	// Use this for initialization
 	void Start () {
 		player = FindObjectOfType<PlayerController> ();
+
+		camera = FindObjectOfType<CameraController> ();
 	}
 	
 	// Update is called once per frame
@@ -34,16 +38,18 @@ public class LevelManager : MonoBehaviour {
 		Instantiate (deathParticle, player.transform.position, player.transform.rotation);
 		player.enabled = false;
 		player.GetComponent<Renderer>().enabled = false;
-		gravityStore = player.GetComponent<Rigidbody2D> ().gravityScale;
-		player.GetComponent<Rigidbody2D>().gravityScale = 0f;
-		player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		camera.isFollowing = false;
+		//gravityStore = player.GetComponent<Rigidbody2D> ().gravityScale;
+		//player.GetComponent<Rigidbody2D>().gravityScale = 0f;
+		//player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 		ScoreManager.AddPoints (-pointPenaltyOnDeath);
 		Debug.Log ("Player Respawn");
 		yield return new WaitForSeconds (respawnDelay);
-		player.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
+		//player.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
 		player.transform.position = currentCheckpoint.transform.position;
 		player.enabled = true;
 		player.GetComponent<Renderer>().enabled = true;
+		camera.isFollowing = true;
 		Instantiate (respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
 	}
 }
