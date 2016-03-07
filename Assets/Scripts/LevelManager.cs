fad1c7912fed8eec7481499f79362009a906d8d5
Daniel Wright <daniel.wright.314@gmail.com>
2016-03-07 08:22:52 -0800

    bounce on the head to kill

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -51,6 +51,7 @@ public class LevelManager : MonoBehaviour {
 		yield return new WaitForSeconds (respawnDelay);
 		//player.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
 		player.transform.position = currentCheckpoint.transform.position;
+		player.knockbackCount = 0;
 		player.enabled = true;
 		player.GetComponent<Renderer>().enabled = true;
 		healthManager.FullHealth ();
