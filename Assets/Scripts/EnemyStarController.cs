36597793e9c8d2194cfb7fd39820023526191e05
Daniel Wright <daniel.wright.314@gmail.com>
2016-03-21 23:01:24 -0700

    enemy shoots crap

---
+++ Assets/Scripts/EnemyStarController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStarController : MonoBehaviour {
+	
+	public float speed;
+	
+	public PlayerController player;
+	
+	public GameObject impactEffect;
+	
+	public float rotationSpeed;
+	
+	public int damageToGive;
+	
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController>();
+		
+		if (player.transform.position.x < transform.position.x) {
+			speed = -speed;
+			rotationSpeed = -rotationSpeed;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
+		
+		GetComponent<Rigidbody2D> ().angularVelocity = rotationSpeed;
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name == "Player") {
+			//Instantiate(enemyDeathEffect, other.transform.position, other.transform.rotation);
+			//Destroy (other.gameObject);
+			//ScoreManager.AddPoints(pointsForKill);
+			
+			HealthManager.hurtPlayer(damageToGive);
+		}
+		
+		Instantiate (impactEffect, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}
